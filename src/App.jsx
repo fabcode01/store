df3c77831394b07301c09cc80af6da7aaca744bb
Fabrício Silva <fabricio85silva@hotmail.com>
2023-11-11 16:50:18 -0300

    fdsfdf

--- src/App.jsx
+++ src/App.jsx
@@ -5,80 +5,50 @@ function App() {
     
 
   return (
-<<<<<<< HEAD
-  <div>
-    
-        <div className="header-bar"></div>
-          <nav>
-            <div className="logo"></div>
-              <label className='search-container'>
-                <input type="text" name="serch-container" id="search-itens" className='search-box'/>
-                <p id='search-placeholder' className='search-placeholder'>O que você procura hoje?</p>
-                <i class="fa-solid fa-magnifying-glass"></i>
-              </label>
-    
-    
-    
-    
-              <i class="fa-regular fa-heart"></i>
-              <span className='user'><i class="fa-regular fa-user"></i></span>
-    
-              <i class="fa-solid fa-bag-shopping"></i>
-    
-    
-          </nav>
-          <h1>Testando</h1>
-          <h3>olá</h3>
-          <h6>opa</h6>
-          <h1>EAE SAUVE</h1>
-    
-  </div>
-    
-=======
+
     <>
-      <div className="header-bar"></div>
-        <nav>
-          <div className="logo"></div>
+    <div className="header-bar"></div>
+      <nav>
+        <div className="logo"></div>
 
-            <label className='search-container'>
-              <input type="text" name="serch-container" id="search-itens" className='search-box'/>
-              <p id='search-placeholder' className='search-placeholder'>O que você procura hoje?</p>
+          <label className='search-container'>
+            <input type="text" name="serch-container" id="search-itens" className='search-box'/>
+            <p id='search-placeholder' className='search-placeholder'>O que você procura hoje?</p>
 
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </label>
-            
-           
-
-
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </label>
           
-          
-            <i class="fa-regular fa-heart"></i>
-            <span className='user'><i class="fa-regular fa-user"></i></span>
-            
-            <i class="fa-solid fa-bag-shopping"></i>
-            
-          
-        </nav>
+         
 
-        <div className="pub">
-              
-          <div className="cep">
-          <i class="fa-solid fa-location-dot"></i>
-          <p>informe seu CEP</p>
+
+        
+        
+          <i class="fa-regular fa-heart"></i>
+          <span className='user'><i class="fa-regular fa-user"></i></span>
+          
+          <i class="fa-solid fa-bag-shopping"></i>
+          
+        
+      </nav>
+
+      <div className="pub">
+            
+        <div className="cep">
+        <i class="fa-solid fa-location-dot"></i>
+        <p>informe seu CEP</p>
+        </div>
+
+        <div className="header-list">
+          <ul className="list">
+            <li className="list-itens">Feminino</li>
+            <li className="list-itens">Masculino</li>
+            <li className="list-itens">Infantil</li>
+          </ul>
+        </div>
           </div>
 
-          <div className="header-list">
-            <ul className="list">
-              <li className="list-itens">Feminino</li>
-              <li className="list-itens">Masculino</li>
-              <li className="list-itens">Infantil</li>
-            </ul>
-          </div>
-            </div>
-
-     
-    </>
->>>>>>> 21e2ea57fd0f0fa98e99eabefc1661c094069ec0
+   
+  </>
   )
 }
 
