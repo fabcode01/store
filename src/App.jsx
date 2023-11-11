4746923500ee022c160db6fb81def43c63602dcc
Fabrício Silva <fabricio85silva@hotmail.com>
2023-11-11 16:43:49 -0300

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,34 @@ function App() {
     
 
   return (
-    <>
-      <div className="header-bar"></div>
-        <nav>
-          <div className="logo"></div>
-
-            <label className='search-container'>
-              <input type="text" name="serch-container" id="search-itens" className='search-box'/>
-              <p id='search-placeholder' className='search-placeholder'>O que você procura hoje?</p>
-
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </label>
-            
-           
-
-
-          
-          
-            <i class="fa-regular fa-heart"></i>
-            <span className='user'><i class="fa-regular fa-user"></i></span>
-            
-            <i class="fa-solid fa-bag-shopping"></i>
-            
-          
-        </nav>
-
-        <h1>Testando</h1>
-        <h3>olá</h3>
-
-        <h6>opa</h6>
-        <h1>EAE SAUVE</h1>
-     
-    </>
+  <div>
+    
+        <div className="header-bar"></div>
+          <nav>
+            <div className="logo"></div>
+              <label className='search-container'>
+                <input type="text" name="serch-container" id="search-itens" className='search-box'/>
+                <p id='search-placeholder' className='search-placeholder'>O que você procura hoje?</p>
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </label>
+    
+    
+    
+    
+              <i class="fa-regular fa-heart"></i>
+              <span className='user'><i class="fa-regular fa-user"></i></span>
+    
+              <i class="fa-solid fa-bag-shopping"></i>
+    
+    
+          </nav>
+          <h1>Testando</h1>
+          <h3>olá</h3>
+          <h6>opa</h6>
+          <h1>EAE SAUVE</h1>
+    
+  </div>
+    
   )
 }
 
